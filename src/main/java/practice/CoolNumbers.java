610ea45812b8be3a67e89744dad996ee1a375c54
Zahar Samborskiy <mafulon@mail.ru>
2022-10-03 20:20:26 +0700

    Homework

--- src/main/java/practice/CoolNumbers.java
+++ src/main/java/practice/CoolNumbers.java
@@ -1,29 +1,74 @@
 package practice;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.*;
 
 public class CoolNumbers {
 
+
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        HashSet<String> list = new HashSet<>();
+        String[] letter = {"А","В","Е","К","М","Н","О","Р","С","Т","У","Х"};
+        // одинаковые буквы
+//        for (int reg = 1; reg < 199; reg++) {
+//            for (String x : letter){
+//                for (int i = 1; i < 999; i++){
+//                    list.add(String.format("%s%d%s%s%d",x,i,x,x,reg));
+//                }
+//            }
+        // одинаковые номера
+        for (String x : letter){
+            for (String y : letter){
+                for (String z : letter){
+                    for (int i = 111; i < 999; i +=111){
+                        for (int reg = 10; reg < 199; reg++){
+                            list.add(String.format("%s%d%s%s%d",x,i,y,z,reg));
+                        }
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(list);
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
+        List<String> listForArray = new ArrayList<>(list);
+        Collections.sort(listForArray);
+        for (String s : listForArray) {
+            if (s.contains(number)){
+                return true;
+            }
+        }
+            return false;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        List<String> list = new ArrayList<>(sortedList);
+        Collections.sort(list);
+        int index = Collections.binarySearch(list, number);
+        if (index < 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
+        for (String s : hashSet) {
+            if (s.equals(number)) {
+                return true;
+            }
+        }
         return false;
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
+        for (String s : treeSet) {
+            if (s.equals(number)){
+                return true;
+            }
+        }
         return false;
     }
 
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,5 +1,8 @@
 package practice;
 
+import java.util.HashSet;
+import java.util.TreeSet;
+
 public class Main {
     /*
     TODO:
@@ -9,6 +12,12 @@ public class Main {
      */
 
     public static void main(String[] args) {
+        CoolNumbers coolNumbers = new CoolNumbers();
+
+        System.out.println(coolNumbers.bruteForceSearchInList(CoolNumbers.generateCoolNumbers(), "А111ВВ15"));
+        System.out.println(coolNumbers.binarySearchInList(CoolNumbers.generateCoolNumbers(), "А111ВВ15"));
+        System.out.println(coolNumbers.searchInHashSet(new HashSet<>(), "А112АВ190"));
+        System.out.println(coolNumbers.searchInTreeSet(new TreeSet<>(), "А112АВ190"));
 
     }
 }
